27c2b86adc770d6196896f97f5677eaceb6b8acf
Sebastian Burckhardt <sburckha@microsoft.com>
2020-09-16 09:08:57 -0700

    missing line

--- src/DurableTask.EventSourced/Abstractions/TransportAbstraction.cs
+++ src/DurableTask.EventSourced/Abstractions/TransportAbstraction.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DurableTask.EventSourced
